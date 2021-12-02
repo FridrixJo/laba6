24458a3514b41cb48a55265b2d676a3203963ab9
kells <pashaglytov2004@mail.ru>
2021-12-02 19:12:22 +0300

    completed

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+void starline() {
+    cout << "\n\n**********************\n\n";
+}
+
+int main()
+{
+    char str[80];
+    char first_part[80];
+    char second_part[80];
+    cout << "str : ";
+    cin.getline(str, 80);
+    int n = 0, p = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i+1] == '.' && isdigit(str[i]) && isdigit(str[i + 2])) {
+            int k = i + 1, m = i + 1;
+            for (int j = k - 1; j > -1; j--) {
+                if (isdigit(str[j]) || str[j] == '-') {
+                   first_part[n] = str[j];
+                   n++;
+                   if (str[j] == '-') {
+
+                       break;
+                   }
+                }
+                else {
+                    break;
+                }
+            }
+            for (int q = m + 1; q < strlen(str); q++) {
+                if (isdigit(str[q]) && str[q] != '.' && str[q] != ',' && str[q] != '=' && str[q] != '+') {
+                    second_part[p] = str[q];
+                    p++;
+                }
+                else {
+                    break;
+                }
+            }
+            break;
+        }else {
+            continue;
+        }
+    }
+    starline();
+    cout << "your value:\t";
+    for (int i = n-1; i > -1; i--) {
+        cout << first_part[i];
+    }
+    cout << '.';
+    for (int i = 0; i < p; i++) {
+        cout << second_part[i];
+    }
+    cout << "\n\nif you get only a dot , then there's no such value in your string:(\n";
+
+    return 0;
+}
---
+++ task2/task2/task2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int CurrentInputDouble() {
+    int a;
+    while (!(cin >> a) || a < INT_MIN || a > INT_MAX) {
+        cin.clear();
+        cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+        cout << "Incorrect input, try again:\n";
+    }
+    return a;
+}
+
+int main()
+{
+    
+
+
+    /*const char* a = "home is the best place where i have ever lived. she was beautiful, like my mum";
+    int s;
+    cin >> s;
+    int i = 0;
+    if ((a[0] != 'a' &&
+        a[0] != 'e' &&
+        a[0] != 'i' &&
+        a[0] != 'o' &&
+        a[0] != 'y' &&
+        a[0] != 'u') && a[s] == ' ') {
+        i = s;
+    }
+    while (a[i] != '\0') {
+        if ((a[i+1] != 'a' &&
+            a[i+1] != 'e' &&
+            a[i+1] != 'i' &&
+            a[i+1] != 'o' &&
+            a[i+1] != 'y' &&
+            a[i+1] != 'u' && a[i] == ' ') && (a[i+s+1] == ' ' || a[i+s+1] == '.' || a[i+s+1] == ',')) {
+            i = i + s + 1;
+            continue;
+        }
+        cout << a[i];
+        i++;
+    }*/
+}
+
