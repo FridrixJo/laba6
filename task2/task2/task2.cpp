b84b8e6f7704b786d2570529bc2b8932bacae412
kells <pashaglytov2004@mail.ru>
2021-12-02 22:24:00 +0300

    completed

--- task2/task2/task2.cpp
+++ task2/task2/task2.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
+#include <cstring>
 using namespace std;
 
-int CurrentInputDouble() {
+int CurrentInput() {
     int a;
     while (!(cin >> a) || a < INT_MIN || a > INT_MAX) {
         cin.clear();
@@ -11,35 +12,44 @@ int CurrentInputDouble() {
     return a;
 }
 
+bool isConsonant(char a) {
+    if (a != 'a' && a != 'A' && a != 'e' && a != 'E' &&
+        a != 'i' && a != 'I' && a != 'y' && a != 'Y' &&
+        a != 'o' && a != 'O' && a != 'u' && a != 'U') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main()
 {
-    
-
-
-    /*const char* a = "home is the best place where i have ever lived. she was beautiful, like my mum";
-    int s;
-    cin >> s;
+    char str[100];
+    cout << "string : ";
+    cin.getline(str, 100);
+    char* ptr = str;
+    cout << "choose the lenght of the word you wanna delete\n";
+    int len = CurrentInput();
     int i = 0;
-    if ((a[0] != 'a' &&
-        a[0] != 'e' &&
-        a[0] != 'i' &&
-        a[0] != 'o' &&
-        a[0] != 'y' &&
-        a[0] != 'u') && a[s] == ' ') {
-        i = s;
+    int end = strlen(ptr);
+    if (ptr[len] == ' ' && isConsonant(ptr[0])) {
+        i = len;
     }
-    while (a[i] != '\0') {
-        if ((a[i+1] != 'a' &&
-            a[i+1] != 'e' &&
-            a[i+1] != 'i' &&
-            a[i+1] != 'o' &&
-            a[i+1] != 'y' &&
-            a[i+1] != 'u' && a[i] == ' ') && (a[i+s+1] == ' ' || a[i+s+1] == '.' || a[i+s+1] == ',')) {
-            i = i + s + 1;
-            continue;
+    if (ptr[end - len - 1] == ' ' && isConsonant(ptr[end-len])) {
+        end -= len;
+    }
+    cout << "your fixed text: ";
+    for (i; i < end; i++) {
+        
+        if (ptr[i] == ' ' && ptr[i+1+len] == ' ' && isConsonant(ptr[i+1])) {
+            i += len;
         }
-        cout << a[i];
-        i++;
-    }*/
+        else {
+            cout << *(ptr+i);
+        }
+    }
+ 
+    return 0;
 }
 
