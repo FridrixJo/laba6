dff48530e402d2d845e067bf385b1b0f23a13fd6
kells <pashaglytov2004@mail.ru>
2021-12-04 18:43:57 +0300

    almost completed

--- task4/task4/task4.cpp
+++ task4/task4/task4.cpp
@@ -2,108 +2,108 @@
 #include <string>
 using namespace std;
 
-void removeUpperLetters(string s) {
+string removeUpperLetters(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'C' || s[i] == 'Q' || s[i] == 'W' || s[i] == 'X') {
             s.erase(i,1);
         }
     }
-
+    return s;
 }
 
-void insteadOf_c(string s) {
+string insteadOf_c(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i+1] == 'c' && (s[i] == 'e' || s[i] == 'i' || s[i] == 'y')) {
             s[i + 1] = 's';
         }
     }
-
+    return s;
 }
 
-void insteadOf_q(string s) {
+string insteadOf_q(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'q') {
             s[i] = 'k';
         }
     }
-
+    return s;
 }
 
-void insteadOf_qu(string s) {
+string insteadOf_qu(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'q' && s[i+1] == 'u') {
             s[i] = 'k';
             s[i + 1] = 'v';
         }
     }
-
+    return s;
 }
 
-void insteadOf_x(string s) {
+string insteadOf_x(string s) {
     int len = s.size();
     for (int i = 0; i < len; i++) {
         if (s[i] == 'x') {
             s.replace(i, 1, "ks");
         }
     }
-
+    return s;
 }
 
-void insteadOf_w(string s) {
+string insteadOf_w(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'w') {
             s[i] = 'v';
         }
     }
-
+    return s;
 }
 
-void insteadOf_ph(string s) {
+string insteadOf_ph(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'p' && s[i+1] == 'h') {
             s.replace(i, 2, "f");
         }
     }
-
+    return s;
 }
 
-void insteadOf_you(string s) {
+string insteadOf_you(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'y' && s[i+1] == 'o' && s[i+2] == 'u') {
             s.replace(i, 3, "u");
         }
     }
-
+    return s;
 }
 
-void insteadOf_oo(string s) {
+string insteadOf_oo(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'o' && s[i + 1] == 'o') {
             s.replace(i, 2, "u");
         }
     }
-
+    return s;
 }
 
-void insteadOf_ee(string s) {
+string insteadOf_ee(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'e' && s[i + 1] == 'e') {
             s.replace(i, 2, "i");
         }
     }
-
+    return s;
 }
 
-void insteadOf_th(string s) {
+string insteadOf_th(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == 't' && s[i + 1] == 'h') {
             s.replace(i, 2, "z");
         }
     }
-
+    return s;
 }
 
-void insteadOf_doubleConsonant(string s) {
+string insteadOf_doubleConsonant(string s) {
     for (int i = 0; i < s.size(); i++) {
         if (s[i] != 'a' && s[i] != 'A' && s[i] != 'e' && s[i] != 'E' &&
             s[i] != 'i' && s[i] != 'I' && s[i] != 'y' && s[i] != 'Y' &&
@@ -115,29 +115,10 @@ void insteadOf_doubleConsonant(string s) {
         }
         
     }
-
+    return s;
 }
 
-//string ChangeLanguage(string s) {
-//    removeUpperLetters(s);
-//    insteadOf_c(s);
-//    insteadOf_q(s);
-//    insteadOf_qu(s);
-//    insteadOf_x(s);
-//    insteadOf_w(s);
-//    insteadOf_ph(s);
-//    insteadOf_you(s);
-//    insteadOf_oo(s);
-//    insteadOf_ee(s);
-//    insteadOf_th(s);
-//    insteadOf_doubleConsonant(s);
-//    return s;
-//}
-
-int main()
-{
-    string s;
-    getline(cin, s);
+string ChangeLanguage(string s) {
     removeUpperLetters(s);
     insteadOf_c(s);
     insteadOf_q(s);
@@ -150,6 +131,14 @@ int main()
     insteadOf_ee(s);
     insteadOf_th(s);
     insteadOf_doubleConsonant(s);
-    cout << s;
+    return s;
+}
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    string s2 = insteadOf_doubleConsonant(s);
+    cout << s2;
     return 0;
 }
