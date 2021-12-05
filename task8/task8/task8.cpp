fd0dedc71ca5cff7534f6afdaaf2db333b37d9cb
kells <pashaglytov2004@mail.ru>
2021-12-05 20:10:41 +0300

    completed

--- task8/task8/task8.cpp
+++ task8/task8/task8.cpp
@@ -18,13 +18,48 @@ bool isItPossible(string s, string t) {
 	return status;
 }
 
-bool itFits(string s, string t) {}
-
-int main() {
+void itFits() {
 	string s;
 	cin >> s;
 	string t;
 	cin >> t;
-	cout << isItPossible(s, t);
+	if (isItPossible(s, t)) {
+		int k = 0;
+		string s1 = "";
+		for (int i = 0; i < s.size(); i++) {
+			if (t[k] == s[i]) {
+				s1.push_back(s[i]);
+				k++;
+			}
+		}
+		for (int i = s.size() - 2; i > -1; i--) {
+			if (t[k] == s[i]) {
+				s1.push_back(s[i]);
+				k++;
+			}
+		}
+		if (s1 == t) {
+			cout << "YES\n";
+		}
+		else {
+			cout << "NO\n";
+		}
+	}
+	else {
+		cout << "NO\n";
+	}
+}
+
+void itFitsQ(int q) {
+	for (int i = 0; i < q; i++) {
+		itFits();
+	}
+}
+
+int main() {
+	int q;
+	cout << "enter q\n";
+	cin >> q;
+	itFitsQ(q);
 	return 0;
 }
