8d2284300153be91a1246d8cd0122a87d8f8e525
kells <pashaglytov2004@mail.ru>
2021-12-05 18:55:18 +0300

    completed

---
+++ task8/task8/task8.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool isItPossible(string s, string t) {
+	bool status = false;
+	int k = 0;
+	while (k < t.size()) {
+		if (s.find(t[k], 0) < 1e9) {
+			status = true;
+			k++;
+		}
+		else {
+			status = false;
+			break;
+		}
+	}
+	return status;
+}
+
+bool itFits(string s, string t) {
+
+}
+
+int main() {
+	string s;
+	cin >> s;
+	string t;
+	cin >> t;
+	cout << isItPossible(s, t);
+	return 0;
+}
--- task9/task9/task9.cpp
+++ task9/task9/task9.cpp
@@ -2,23 +2,43 @@
 #include <string>
 using namespace std;
 
+int countNum(string s) {
+    int a[100];
+    int m = 0;
+    for (int i = s.size() - 1; i > -1; i--) {
+        if (isdigit(s[i])) {
+            int k = 0;
+            int res = 0;
+            for (int j = i; j > -1; j--) {
+                if (isdigit(s[j])) {
+                    res += static_cast<int>(s[j]-48) * pow(10, k);
+                    k++;
+                }
+                else {
+                    break;
+                }
+            }
+            a[m] = res;
+            m++;
+            i -= k;
+        }
+        else {
+            continue;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < m; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+
+
 int main()
 {
     string s;
-    getline(cin, s);
-    double sum = 0;
-    for (int i = 0; i < s.size(); i++) {
-        if (isdigit(s[i])) {
-            int counter = 0;
-            for (int j = i + 1; isdigit(s[i]); j++) {
-                counter++;
-            }
-            for (int j = 0; j <= counter; j++) {                    
-                sum += pow(10, counter - j) * s[i + j];
-            }
-            i += counter;
-        }
-    }
-    cout << sum;
+    cin >> s;
+    cout << "the sum of all numbers int the string : " << countNum(s);
     return 0;
 }
